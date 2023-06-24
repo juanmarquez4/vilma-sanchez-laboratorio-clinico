a890ace217211f697ee3ae1d8f62f70584cc2b18
juanmarquez4 <67566810+juanmarquez4@users.noreply.github.com>
2023-06-24 11:31:39 +1000

    update branch

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
